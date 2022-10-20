362118fa9b544b8c4db874b916ad53931a0814ab
Alan DeGuzman <alandeguz@users.noreply.github.com>
2022-10-20 11:06:46 -0500

    update

--- Package.swift
+++ Package.swift
@@ -20,12 +20,3 @@ let package = Package(
       checksum: "274eae6001e6649dd70a7bef6aa60b01bd9000c223d53560352d6ad591b6a874")
   ]
 )
-
-//url: "https://github.com/quentinfasquel/R.swift/releases/download/6.0.0-spm/rswift.artifactbundle.zip",
-//checksum: "357e8982f00e17a8d40027a40c1e01cfd90d5345ce0e30ae16fe78cba49704a7"
-
-// url: "https://github.com/alandeguz/swift-build-tools/releases/download/1.0.4/RswiftBinary-macos.artifactbundle.zip",
-// checksum: "274eae6001e6649dd70a7bef6aa60b01bd9000c223d53560352d6ad591b6a874"
-
-// url: "https://github.com/alandeguz/R.swift/releases/download/v6.1.0/RswiftBinary-macos.artifactbundle.zip"
-// checksum: "3957f1f633f5d82de1dd5535abc1d02cd3758f4284dd7cffd613240732c04bc5"
